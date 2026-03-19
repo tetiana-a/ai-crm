4e5700d8611f4a0e363aca1a73d79351ab233c13
Tetiana Kotolup <93287415+tetiana-a@users.noreply.github.com>
2026-03-19 07:38:47 +0100

    Update aiController.js

--- backend/src/controllers/aiController.js
+++ backend/src/controllers/aiController.js
@@ -1,4 +1,4 @@
-import { pool } from '../config/db.js';
+import pool from '../config/db.js';
 import { generateBusinessInsight, generateReply } from '../services/openaiService.js';
 
 function languageMap(code) {
